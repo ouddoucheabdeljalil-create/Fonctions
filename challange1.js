b656b77b1166385713f7ddc05840c8c6b30ab1fe
unknown <ouddoucheabdeljalil@gmail.com>
2026-09-22 17:24:15 +0200

    Fonction de Somme

---
+++ challange1.js
@@ -0,0 +1,6 @@
+const prompt = require('prompt-sync')();
+
+function addition(num1,num2) {
+    return num1 + num2 ;
+}
+console.log(addition(Number(prompt("entez premier numero : ")), Number(prompt("entez deuxieme numero : "))));
